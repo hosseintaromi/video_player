b766bb8465ad7e8ef7a6fef62bd46361ed968ef5
hossein taromi <hosseintaromii@gmail.com>
2023-09-10 14:57:35 +0330

    ci: :bug: fix publish bug

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+
 import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
@@ -25,6 +26,7 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+
       postcss(),
     ],
   },
@@ -32,6 +34,7 @@ export default [
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+
     external: [/\.css$/],
   },
 ];
