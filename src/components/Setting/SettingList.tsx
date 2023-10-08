51359662b74515ba4198aa4c818fbfd93561a9cd
hossein taromi <hosseintaromii@gmail.com>
2023-10-08 17:58:27 +0330

    feat: :white_check_mark: add all item for setting list

--- src/components/Setting/SettingList.tsx
+++ src/components/Setting/SettingList.tsx
@@ -6,6 +6,8 @@ import { IconWrapper, SettingMenu } from '../General/FlexCenter';
 import ArrowRight from '../assets/Icons/ArrowRight';
 import ChangeQuality from '../assets/Icons/ChangeQuality';
 import { pageDir, pageName } from './Setting';
+import Subtitle from '../assets/Icons/Subtitle';
+import AudioIcon from '../assets/Icons/AudioIcon';
 
 
 const SettingList = ({ changePage, myRef }: { changePage: (newPageName: pageName, dir: pageDir) => void, myRef: React.RefObject<HTMLDivElement> }) => {
@@ -25,6 +27,26 @@ const SettingList = ({ changePage, myRef }: { changePage: (newPageName: pageName
                 <SettingItem startIcon={<ChangeQuality />} content='ChangeQuality'>
                     <span>
                         720
+                    </span>
+                    <IconWrapper>
+                        <ArrowRight />
+                    </IconWrapper>
+                </SettingItem>
+            </div>
+            <div onClick={() => changePage(pageName.quality, pageDir.forward)}>
+                <SettingItem startIcon={<Subtitle />} content='Subtitles / cc'>
+                    <span>
+                        English
+                    </span>
+                    <IconWrapper>
+                        <ArrowRight />
+                    </IconWrapper>
+                </SettingItem>
+            </div>
+            <div onClick={() => changePage(pageName.quality, pageDir.forward)}>
+                <SettingItem startIcon={<AudioIcon />} content='Audio Track'>
+                    <span>
+                        English
                     </span>
                     <IconWrapper>
                         <ArrowRight />
