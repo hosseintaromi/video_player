a34b4f4da421ee9f0920ec5e9b9d1da037f94f0f
hossein <hosseintaromii@gmail.com>
2023-10-21 14:58:08 +0330

    feat: :sparkles: picture in picture in toolbar #40

---
+++ src/components/Icons/PictureInPicture.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const PictureInPicture = () => {
+    return (
+        <svg stroke="currentColor" fill="none" strokeWidth="1.5" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="30px" width="30px" xmlns="http://www.w3.org/2000/svg"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M11 19h-6a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v4"></path><path d="M14 14m0 1a1 1 0 0 1 1 -1h5a1 1 0 0 1 1 1v3a1 1 0 0 1 -1 1h-5a1 1 0 0 1 -1 -1z"></path><path d="M7 9l4 4"></path><path d="M8 13h3v-3"></path></svg>)
+}
+
+export default PictureInPicture
