45be84ab1f6e292e61b85508def67366e81d3486
hossein <hosseintaromii@gmail.com>
2023-12-12 16:38:01 +0330

    feat: :sparkles: one click toggle play pause and double click toggle fullscreen

--- src/components/player/TouchContainer.tsx
+++ src/components/player/TouchContainer.tsx
@@ -1,14 +1,28 @@
-import React, { ReactNode, useCallback, useRef } from 'react'
+import React, { ReactNode, useCallback, useRef, useState } from 'react'
 import { usePlayerContext } from '../../hooks/usePlayerContext'
 import { throttle } from "lodash-es"
+import { useFullscreen } from '../../hooks/useFullscreen'
 
 const TouchContainer = ({ children, onShow }: { children: ReactNode, onShow: (show: boolean) => void }) => {
     const isPlay = useRef<boolean>()
     const isShow = useRef<boolean>()
     const timeOutRef = useRef<NodeJS.Timeout | undefined>()
 
-    const { getHideTime } = usePlayerContext({
+
+    const video_wrapper_id = document.getElementById("video_wrapper_id");
+    const video_player = document.getElementById("video_player");
+
+    const { toggleFullscreen } = useFullscreen((e) => {
+        (window.screen.orientation as any)?.lock("landscape-primary");
+    }, video_wrapper_id, video_player);
+
+    const togglePlay = () => {
+        changePlayPause(!isPlay.current)
+    }
+
+    const { getHideTime, changePlayPause } = usePlayerContext({
         onPlayPause: (playStatus: boolean) => {
+            console.log('first', playStatus)
             isPlay.current = playStatus
             hideWithDelay()
         }
@@ -41,6 +55,8 @@ const TouchContainer = ({ children, onShow }: { children: ReactNode, onShow: (sh
     const calcThrottle = useCallback(throttle(hideWithDelay, 200), [])
     return (
         <div
+            onDoubleClick={toggleFullscreen}
+            onClick={togglePlay}
             onMouseMove={calcThrottle}
             onTouchEnd={calcThrottle}
             onTouchMove={calcThrottle}
