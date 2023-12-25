cc5579369c053d06716c2ca69e6f6d9fb9102786
katebmedia <katebmedia@gmail.com>
2023-12-25 09:45:29 +0330

    feat: :sparkles: modal added to project

--- src/components/general/DialogStyle.ts
+++ src/components/general/DialogStyle.ts
@@ -13,17 +13,33 @@ export const ModalOverlay = styled.div({
 });
 
 export const ModalContent = styled.div({
-  position: "relative",
-  backgroundColor: "#fff",
-  border: "1px solid rgba(0,0,0,0.3)",
-  backgroundClip: "padding-box",
-  borderRadius: "0.3rem",
-  padding: "1rem",
+    boxShadow: "0 0 24px 12px rgba(0,0,0,0.25)",
+    borderRadius: "12px",
+    position: "relative",
+    zIndex: 2,
+    display: "flex",
+    flexDirection: "column",
+    padding: "16px",
+    margin: "0 auto",
+    overflowX: "hidden",
+    overflowY: "auto",
+    fontSize: "1.3rem",
+    color: "#0f0f0f",
+    backgroundColor: "#fff",
+    border:" none",
+    minWidth: "250px",
+    maxWidth: "356px",
+    maxHeight: "100%",
 });
 
 export const ModalWrapper = styled.div({
-  maxWidth: "500px",
-  margin: "2rem auto",
-  zIndex: 20,
-  position: 'relative'
+    position: "fixed",
+    top: "0",
+    left: "0",
+    right: "0",
+    bottom: "0",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 2,
 });
