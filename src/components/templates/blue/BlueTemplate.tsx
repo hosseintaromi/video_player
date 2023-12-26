14914313a5134e28726eba38e3f6485a2fab9c93
hossein <hosseintaromii@gmail.com>
2023-12-26 17:32:16 +0330

    feat: :zap: fix hide bug

--- src/components/templates/blue/BlueTemplate.tsx
+++ src/components/templates/blue/BlueTemplate.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from '@emotion/react'
 import { Gradient, PlayJumpIcon, PlayJumpIconFix, PlayJumpIconWrapper, VideoWrapperBlue } from '../../player/VideoPlayerStyle';
 import Loading from '../../loading/Loading';
@@ -10,20 +10,15 @@ import { usePlayerContext } from '../../../hooks/usePlayerContext';
 import JumpBack from '../../tools/JumpBack';
 import JumpForward from '../../tools/JumpForward';
 import BlueToolbar from '../../toolbar/BlueToolbar';
-import Icon from '../../icons/Icon';
 
 const BlueTemeplate = () => {
     const { style } = useStyle();
     const [isFadeOut, setIsFadeOut] = useState<boolean>(false);
-    const [isPlay, setIsPlay] = useState<boolean>(false);
-    usePlayerContext({
-        onPlayPause: (playStatus: boolean) => {
-            setIsPlay(true)
-            setTimeout(() => {
-                setIsPlay(false)
-            }, 450);
-        }
-    })
+
+
+    useEffect(() => {
+        console.log('firstdddd', isFadeOut)
+    }, [isFadeOut])
     return (
         <ThemeProvider theme={style}>
             <VideoWrapperBlue id="video_wrapper_id">
@@ -35,7 +30,7 @@ const BlueTemeplate = () => {
                     <JumpForward />
                 </PlayJumpIconWrapper>
                 <Loading />
-                <TouchContainer canPlayOnClick={false} onShow={(show: boolean) => { setIsFadeOut(!show) }} >
+                <TouchContainer canPlayOnClick={false} onShow={(show: boolean) => { console.log('show', show); setIsFadeOut(!show) }} >
                     <Video />
                 </TouchContainer>
                 <Gradient isFaded={isFadeOut} />
