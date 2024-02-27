239ea6b9e3be96c2281936d4038b21f2693dbaaf
hossein <hosseintaromii@gmail.com>
2024-02-27 12:18:42 +0330

    usePlayerEvents

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { usePlayer } from "./usePlayer";
 export { usePlayerContext } from "./usePlayerContext";
 export { useFullscreen } from "./useFullscreen";
+export { usePlayerEvents } from "./usePlayerEvents";
