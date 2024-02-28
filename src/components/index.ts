35a5c9663ee3699186578c64824e0970483496de
hossein <hosseintaromii@gmail.com>
2024-02-28 14:59:49 +0330

    time

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,7 @@ export { default as TouchContainer } from "./player/TouchContainer";
 export { default as Video } from "./player/Video";
 export { default as TimeLine } from "./timeline/MediaTimeLine";
 export { default as Fullscreen } from "./tools/Fullscreen";
+export { default as Time } from "./tools/Time";
 export { default as Jump } from "./tools/Jump";
 export { default as Volume } from "./tools/Volume";
 
