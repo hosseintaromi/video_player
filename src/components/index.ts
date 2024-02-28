62e0db2e9eb65da3c42df59123dce0cc1b9fa442
hossein <hosseintaromii@gmail.com>
2024-02-28 15:28:00 +0330

    mute

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,6 +9,8 @@ export { default as Fullscreen } from "./tools/Fullscreen";
 export { default as Time } from "./tools/Time";
 export { default as Jump } from "./tools/Jump";
 export { default as Volume } from "./tools/Volume";
+export { default as Mute } from "./tools/Mute";
+
 
 
 export { CustomPlayWrapper, CustomVideoWrapper } from "./player/VideoPlayerStyle";
