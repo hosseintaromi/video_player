b51e0e7ca760df23228d3a95cb3b22e35667fcdf
hossein <hosseintaromii@gmail.com>
2024-03-05 14:25:49 +0330

    test for subtitle

--- src/components/player/Video.tsx
+++ src/components/player/Video.tsx
@@ -10,57 +10,32 @@ const VideoTag = styled.video({
     backgroundColor: '#000'
 });
 const Video = () => {
-    const { setVideoRef, autoPlay, changeMute } = usePlayerContext()
-    const videoRef = useRef<HTMLVideoElement>(null)
+    const { setVideoRef, autoPlay } = usePlayerContext()
     const [mode, setMode] = useState('')
+    const videoRef = useRef<HTMLVideoElement>(null)
     useEffect(() => {
-
-
-
-
         setVideoRef?.(videoRef.current!)
-        const vid = document.getElementById("reza") as HTMLVideoElement;
-        if (vid)
-            for (let i = 0; i < vid.textTracks.length; i++) {
-                console.log(vid.textTracks[i].mode)
-
-                setMode(vid.textTracks[i].mode)
-            }
-
     }, [])
 
-    const showMe = () => {
+    // const showMe = () => {
 
-        console.log('first')
-        let trackElem = document.querySelector("track")!;
-        let track = trackElem.track as any;
-        track.mode = "showing";
+    //     console.log('first')
+    //     let trackElem = document.querySelector("track")!;
+    //     let track = trackElem.track as any;
+    //     track.mode = "showing";
 
-        for (const cue of track.cues) {
+    //     for (const cue of track.cues) {
 
-            cue.pauseOnExit = true;
-        }
+    //         cue.pauseOnExit = true;
+    //     }
 
-    }
+    // }
 
     return (
-        <>
 
-
-            {/* <video id='reza' width="600" height="400" controls crossOrigin='anonymous'>
-                <source src="https://brenopolanski.github.io/html5-video-webvtt-example/MIB2.mp4" type="video/mp4" />
-                <track label="en" kind="subtitles" srcLang="en" src={'https://gotranscript.com/samples/captions-example.srt'} />
-                <track label="fa" kind="subtitles" srcLang="fa" src="https://brenopolanski.github.io/html5-video-webvtt-example/MIB2-subtitles-pt-BR.vtt" />
-            </video> */}
-            <button style={{ position: 'fixed', zIndex: '10000000' }} onClick={showMe}>
-                {mode}
-            </button>
-
-            <VideoTag ref={videoRef} autoPlay={autoPlay} playsInline muted id='video_player' crossOrigin='anonymous' controls>
-                <track label="p2" kind="subtitles" srcLang="en" src="https://brenopolanski.github.io/html5-video-webvtt-example/MIB2-subtitles-pt-BR.vtt" default />
-            </VideoTag>
-
-        </>
+        <VideoTag ref={videoRef} autoPlay={autoPlay} playsInline muted id='video_player' crossOrigin='anonymous'>
+            {/* <track label="p2" kind="subtitles" srcLang="en" src="https://brenopolanski.github.io/html5-video-webvtt-example/MIB2-subtitles-pt-BR.vtt" default /> */}
+        </VideoTag>
     )
 }
 
