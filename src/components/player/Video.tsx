8458fe421fe95399aad30f8840e56f9d22c00e4b
hossein <hosseintaromii@gmail.com>
2024-03-05 11:31:59 +0330

    test for subtitle

--- src/components/player/Video.tsx
+++ src/components/player/Video.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from 'react'
 import { usePlayerContext } from '../../hooks/usePlayerContext'
 import styled from '@emotion/styled'
 
+
 const VideoTag = styled.video({
     width: "100%",
     height: "100%",
@@ -14,15 +15,32 @@ const Video = () => {
 
     useEffect(() => {
         setVideoRef?.(videoRef.current!)
-
+        // const vid = document.getElementById("reza") as HTMLVideoElement;
+        // console.log(vid?.textTracks)
+        // if (videoRef.current)
+        //     for (let i = 0; i < videoRef.current.textTracks.length; i++) {
+        //         console.log(videoRef.current.textTracks[i].mode)
+        //         videoRef.current.textTracks[i].mode = "showing";
+        //     }
         setTimeout(() => {
             //changeMute(false)
         }, 2000);
     }, [])
 
     return (
-        // <VideoTag ref={videoRef} autoPlay={autoPlay} playsInline muted id='video_player' />
-        <VideoTag ref={videoRef} id='video_player' />
+        <>
+
+            <video id='reza' width="600" height="400" controls crossOrigin='anonymous'>
+                <source src="https://brenopolanski.github.io/html5-video-webvtt-example/MIB2.mp4" type="video/mp4" />
+                <track label="pt" kind="subtitles" srcLang="en" src={'https://gotranscript.com/samples/captions-example.srt'} default />
+
+                <track label="pt" kind="subtitles" srcLang="en" src="https://brenopolanski.github.io/html5-video-webvtt-example/MIB2-subtitles-pt-BR.vtt" default />
+            </video>
+            {/* <VideoTag ref={videoRef} autoPlay={autoPlay} playsInline muted id='video_player' crossOrigin='anonymous' controls>
+                <track label="p1" kind="subtitles" srcLang="en" src={'https://gotranscript.com/samples/captions-example.srt'} default />
+                <track label="p2" kind="subtitles" srcLang="en" src="https://brenopolanski.github.io/html5-video-webvtt-example/MIB2-subtitles-pt-BR.vtt" default />
+            </VideoTag> */}
+        </>
     )
 }
 
